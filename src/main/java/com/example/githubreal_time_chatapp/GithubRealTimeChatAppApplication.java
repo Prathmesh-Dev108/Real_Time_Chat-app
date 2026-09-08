dc4d7ad7a0052db11f2e6c1ec17db43e85155e0a
DevPrathmesh <prathmeshausofficial774@gmail.com>
2026-09-08 11:57:13 +0530

    init commit

---
+++ src/main/java/com/example/githubreal_time_chatapp/GithubRealTimeChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubreal_time_chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRealTimeChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRealTimeChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubreal_time_chatapp/GithubRealTimeChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubreal_time_chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRealTimeChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
